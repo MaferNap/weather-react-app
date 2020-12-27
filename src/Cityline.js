c19faf69773011ff8592754b955513464733c13e
MaferNap <MaferNap@users.noreply.github.com>
2020-12-26 22:33:30 -0600

    Update Skyline image component

--- src/Cityline.js
+++ src/Cityline.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./Cityline.css";
+import Skyline from "./skylinecity1.png";
 
 export default function Cityline() {
   return (
     <div className="cityline">
-      <img alt="skyline" src={require("./skylinecity1.png")} />
+      <img alt="skyline" src={Skyline} />
     </div>
   );
 }
