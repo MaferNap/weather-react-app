897f91ba883edd331c2b09d6d6fc48133338713a
MaferNap <MaferNap@users.noreply.github.com>
2020-12-26 22:36:44 -0600

    Update World image component

--- src/World.js
+++ src/World.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./World.css";
+import Globe from "./globetransparent.png";
 
 export default function World() {
   return (
     <div className="world">
-      <img alt="globe" src={require("./globetransparent.png")} />
+      <img alt="globe" src={Globe} />
     </div>
   );
 }
