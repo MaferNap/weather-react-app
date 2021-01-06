8813c3c3688bbf8aab9ae6c4568e985be90857e9
MaferNap <MaferNap@users.noreply.github.com>
2021-01-06 01:47:16 -0600

    Update weather.js component

--- src/Weather.js
+++ src/Weather.js
@@ -1,12 +1,37 @@
 import React from "react";
-import Search from "./Search";
+
 import TemperatureDisplay from "./TemperatureDisplay";
 import Forecast from "./Forecast";
 
 export default function Weather() {
   return (
     <div className="col-9">
-      <Search />
+      <form className="weatherSearch" id="city-search">
+        <input
+          className="cityBox"
+          type="text"
+          placeholder="Enter City..."
+          autoComplete="off"
+          autoFocus="on"
+          id="city-quest"
+        />
+
+        <input
+          className="searchbutton"
+          type="submit"
+          value="Search"
+          id="city-search-button"
+        />
+
+        <button className="locationButton" id="current-location-button">
+          Current Location{" "}
+          <span role="img" aria-label="pin">
+            {" "}
+            📍{" "}
+          </span>
+        </button>
+      </form>
+
       <TemperatureDisplay />
       <Forecast />
     </div>
