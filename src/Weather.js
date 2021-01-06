e726dca8a10051adc107e35f6ad6d6ff8c08a238
MaferNap <MaferNap@users.noreply.github.com>
2021-01-06 16:46:53 -0600

    Modify apiUrl in Weather.js component

--- src/Weather.js
+++ src/Weather.js
@@ -103,7 +103,7 @@ export default function Weather(props) {
     const apiKey = "88d9871371b1db4131f1d79918fff4e1";
 
     let units = "metric";
-    let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=${units}`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=${units}`;
     axios.get(apiUrl).then(handleResponse);
 
     return "Loading...";
